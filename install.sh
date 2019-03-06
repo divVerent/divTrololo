22778f025dc10edc5dd5881a3b35c819f7c0f4ad
Rudolf Polzer <divVerent@gmail.com>
2019-03-05 20:24:02 -0500

    Move to a better way to make the levels in this repo editable.

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+root="$HOME/.local/share/supertux2"
+
+if ! [ -d "$root" ]; then
+	echo >&2 "SuperTux hasn't run yet. Please start and quit SuperTux first"
+	echo >&2 "to confirm the data location!"
+	exit 1
+fi
+
+set -ex
+mkdir -p "$root/levels"
+ln -snf "$PWD/levels/divTrololo" "$root/levels/divTrololo"
--- zipit-divTrololo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -f ../divTrololo.zip
-7za a -tzip -mx=9 -mfb=257 -mpass=9 -r ../divTrololo.zip \
-	divTrololo.nfo divTrololo/
